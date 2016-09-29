8fb436199980bf40b341bb400d86d9ea307c6b91
Rishit Sinha <rishitsinha25@gmail.com>
2016-09-29 18:16:39 +0530

    Added Class - Task
    Class task is supposed to contain functions to assign, edit and keep
    track of all the assignments given by the head to subheads. It includes
    functions to communicate with a table task to store information.

---
+++ portal/class.task.php
@@ -0,0 +1,115 @@
+<?php
+
+require_once('dbconfig.php');
+
+class TASK
+{	
+
+	private $conn;
+	private function engage($uname){
+		try
+		{
+			$stmt = $this->conn->prepare("UPDATE user SET engaged = '1' WHERE user_name = :uname");
+
+			$stmt->bindparam(":uname",$uname);
+			$stmt->execute();
+		}
+		catch(PDOException $e)
+		{
+			echo $e->getMessage();
+		}
+	}
+
+	private function disengage($uname){
+		try
+		{
+			$stmt = $this->conn->prepare("UPDATE user SET engaged = '0' WHERE user_name = :uname");
+
+			$stmt->bindparam(":uname",$uname);
+			$stmt->execute();
+		}
+		catch(PDOException $e)
+		{
+			echo $e->getMessage();
+		}
+	}
+
+	public function __construct()
+	{
+		$database = new Database();
+		$db = $database->dbConnection();
+		$this->conn = $db;
+    }
+	
+	public function runQuery($sql)
+	{
+		$stmt = $this->conn->prepare($sql);
+		return $stmt;
+	}
+
+	public function getActiveTasks()
+	{
+		try
+		{
+			$stmt = $this->conn->prepare("SELECT task.task_by , task.task_desc , task.task_for , user.full_name FROM task JOIN user on (user.user_id = task.task_by) WHERE task.completed=0");
+			$stmt->execute();
+			while($taskRow=$stmt->fetch(PDO::FETCH_OBJ)){
+				echo $taskRow->full_name." assigned the task: ".$taskRow->task_desc." to ".$taskRow->task_for,'<br>';
+			}
+		}
+		catch(PDOException $e)
+		{
+			echo $e->getMessage();
+		}
+	}
+
+	public function getCompletedTasks()
+	{
+		try
+		{
+			$stmt = $this->conn->prepare("SELECT task.task_by , task.task_desc , task.task_for , user.user_name FROM task JOIN user on (user.user_id = task.task_by) WHERE task.completed=1");
+			$stmt->execute();
+			while($taskRow=$stmt->fetch(PDO::FETCH_OBJ)){
+				echo $taskRow->user_name." assigned the task: ".$taskRow->task_desc." to ".$taskRow->task_for,'<br>';
+			}
+		}
+		catch(PDOException $e)
+		{
+			echo $e->getMessage();
+		}
+	}
+
+	public function assignTask($hname, $shname, $task, $adate, $ddate)
+	{
+
+		try
+		{
+			$stmt = $this->conn->prepare("INSERT INTO task(task_by,task_for,task_desc,date_assigned,date_completed) 
+		                                               VALUES(:hname, :shname, :task, :adate, :ddate)");
+												  
+			$stmt->bindparam(":hname", $hname);
+			$stmt->bindparam(":shname", $shname);
+			$stmt->bindparam(":task", $task);
+			$stmt->bindparam(":adate", $adate);
+			$stmt->bindparam(":ddate", $ddate);
+													  
+				
+			$stmt->execute();
+			$this->disengage($shname);
+			return $stmt;
+			
+		}
+		catch(PDOException $e)
+		{
+			echo $e->getMessage();
+		}
+	}
+
+	
+	
+	public function redirect($url)
+	{
+		header("Location: $url");
+	}
+}
+?>
