7f4d2a633a2744ec83cde7a6c1da77904d8167ee
Abhinav Saini <sainiabhinav75@gmail.com>
2016-09-29 17:07:16 +0530

    Login And LogOut
    Different Logins for head and subheads, Append the work asssigned by heads

---
+++ subhead.php
@@ -0,0 +1,76 @@
+<?php
+
+	require_once("session.php");
+	
+	require_once("class.user.php");
+	$auth_user = new USER();
+	
+	
+	$user_id = $_SESSION['user_session'];
+	
+	$stmt = $auth_user->runQuery("SELECT * FROM login WHERE user_id=:user_id");
+	$stmt->execute(array(":user_id"=>$user_id));
+	$userRow=$stmt->fetch(PDO::FETCH_ASSOC);
+  
+  $stmts = $auth_user->runQuery("SELECT * FROM login, detail WHERE user_id=assigned_by AND assigned_to= :user_id");
+  $stmts->execute(array(":user_id"=>$user_id));
+  ?>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+<link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" media="screen">
+<script type="text/javascript" src="jquery-1.11.3-jquery.min.js"></script>
+<link rel="stylesheet" href="style.css" type="text/css"  />
+<title>welcome</title>
+</head>
+
+<body>
+
+          <ul class="nav navbar-nav navbar-right">
+            
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+			  <span class="glyphicon glyphicon-user"></span>&nbsp;Hi <?php echo $userRow['full_name']; ?>&nbsp;<span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="logout.php?logout=true"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Sign Out</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+
+    <div class="clearfix"></div>
+    	
+    
+<div class="container-fluid" style="margin-top:80px;">
+	
+    <div class="container">
+    
+    	<label class="h5">Welcome : <?php print($userRow['full_name']); ?></label>
+        <hr />
+        <?php 
+        while($userRows=$stmts->fetch()){     //Append the work Assigned to him
+          if($userRows['completed']==0)
+            { 
+              echo '<font color="red">';      // Incomplete work will be red 
+            } 
+            else {
+            echo '<font color="green">';      //Completed work will be green
+          }
+          ?>
+      <label class="h5">You have been Assigned <?php print($userRows['work']); ?> </label>
+      <label class="h5">By <?php print($userRows['full_name']); ?></label></font>
+        <hr />
+         <?php }?>
+    
+    </div>
+
+</div>
+
+<script src="bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+</html>
