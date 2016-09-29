f4f37f042a7a2715449bb04b31d352455805d333
sainiabhinav <sainiabhinav75@gmail.com>
2016-09-29 15:16:59 +0530

    Login and Logout
    login for heads and subheads

---
+++ portal/class.user.php
@@ -0,0 +1,94 @@
+<?php
+
+require_once('dbconfig.php');
+
+class USER
+{	
+
+	private $conn;
+	
+	public function __construct()
+	{
+		$database = new Database();
+		$db = $database->dbConnection();
+		$this->conn = $db;
+    }
+	
+	public function runQuery($sql)
+	{
+		$stmt = $this->conn->prepare($sql);
+		return $stmt;
+	}
+	public function checkHead($uname)
+	{
+		try
+		{
+			$stmt = $this->conn->prepare("SELECT * FROM login WHERE user_name=:uname ");
+			$stmt->execute(array(':uname'=>$uname));
+			$userRow=$stmt->fetch(PDO::FETCH_ASSOC);
+			if($stmt->rowCount() == 1)
+			{
+				if( $userRow['head'])
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+		}
+		catch(PDOException $e)
+		{
+			echo $e->getMessage();
+		}
+	}
+	
+	
+	public function doLogin($uname,$upass)
+	{
+		try
+		{
+			$stmt = $this->conn->prepare("SELECT * FROM login WHERE user_name=:uname ");
+			$stmt->execute(array(':uname'=>$uname));
+			$userRow=$stmt->fetch(PDO::FETCH_ASSOC);
+			if($stmt->rowCount() == 1)
+			{
+				if($upass== $userRow['user_pass'])
+				{
+					$_SESSION['user_session'] = $userRow['user_id'];
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+		}
+		catch(PDOException $e)
+		{
+			echo $e->getMessage();
+		}
+	}
+	
+	public function is_loggedin()
+	{
+		if(isset($_SESSION['user_session']))
+		{
+			return true;
+		}
+	}
+	
+	public function redirect($url)
+	{
+		header("Location: $url");
+	}
+	
+	public function doLogout()
+	{
+		session_destroy();
+		unset($_SESSION['user_session']);
+		return true;
+	}
+}
+?>
---
+++ portal/portal.sql
@@ -0,0 +1,99 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 29, 2016 at 11:44 AM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.5.37
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `portal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail`
+--
+
+CREATE TABLE `detail` (
+  `sNo` int(11) NOT NULL,
+  `assigned_to` int(11) NOT NULL,
+  `work` varchar(255) NOT NULL,
+  `assigned_by` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `detail`
+--
+
+INSERT INTO `detail` (`sNo`, `assigned_to`, `work`, `assigned_by`) VALUES
+(1, 2, 'ANY', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `user_pass` varchar(255) NOT NULL,
+  `full_name` text NOT NULL,
+  `engagged` tinyint(1) NOT NULL,
+  `head` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`user_id`, `user_name`, `user_pass`, `full_name`, `engagged`, `head`) VALUES
+(1, 'harsh', 'harshB123', 'Harsh Bajaj', 0, 1),
+(2, 'rishit', 'rishit123', 'Rishit Sinha', 0, 0),
+(3, 'abhinav', 'abhinav123', 'Abhinav Saini', 0, 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `detail`
+--
+ALTER TABLE `detail`
+  ADD PRIMARY KEY (`sNo`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `detail`
+--
+ALTER TABLE `detail`
+  MODIFY `sNo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
