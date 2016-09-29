3ccc40c36430e1cf5d30cd07dad9453c62ba3a98
sainiabhinav <sainiabhinav75@gmail.com>
2016-09-29 15:50:16 +0530

    Changes in head's portal
    Appends the works that are assigned to different subheads by him

--- portal/home.php
+++ portal/home.php
@@ -1,23 +1,20 @@
 <?php
 
-	require_once("session.php");
-	
-	require_once("class.user.php");
-	$auth_user = new USER();
-	
-	
-	$user_id = $_SESSION['user_session'];
-	
-	$stmt = $auth_user->runQuery("SELECT * FROM login WHERE user_id=:user_id");
-	$stmt->execute(array(":user_id"=>$user_id));
-	$userRow=$stmt->fetch(PDO::FETCH_ASSOC);
-
-    $stmts = $auth_user->runQuery("SELECT * FROM login, detail WHERE assigned_by=:user_id");
+  require_once("session.php");
+  
+  require_once("class.user.php");
+  $auth_user = new USER();
+  
+  
+  $user_id = $_SESSION['user_session'];
+  
+  $stmt = $auth_user->runQuery("SELECT * FROM login WHERE user_id=:user_id");
+  $stmt->execute(array(":user_id"=>$user_id));
+  $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
+  
+  $stmts = $auth_user->runQuery("SELECT * FROM login, detail WHERE user_id=assigned_to AND assigned_by= :user_id");
   $stmts->execute(array(":user_id"=>$user_id));
-  $userRows=$stmts->fetch(PDO::FETCH_ASSOC);
-    $stmtss = $auth_user->runQuery("SELECT * FROM login, detail WHERE user_id=assigned_to");
-  $userRowss=$stmts->fetch(PDO::FETCH_ASSOC);
-?>
+  ?>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -30,53 +27,37 @@
 
 <body>
 
-<nav class="navbar navbar-default navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="http://www.codingcage.com">Coding Cage</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="http://www.codingcage.com/2015/04/php-login-and-registration-script-with.html">Back to Article</a></li>
-            <li><a href="http://www.codingcage.com/search/label/jQuery">jQuery</a></li>
-            <li><a href="http://www.codingcage.com/search/label/PHP">PHP</a></li>
-          </ul>
           <ul class="nav navbar-nav navbar-right">
             
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-			  <span class="glyphicon glyphicon-user"></span>&nbsp;Hi' <?php echo $userRow['full_name']; ?>&nbsp;<span class="caret"></span></a>
+        <span class="glyphicon glyphicon-user"></span>&nbsp;Hi' <?php echo $userRow['full_name']; ?>&nbsp;<span class="caret"></span></a>
               <ul class="dropdown-menu">
                 <li><a href="profile.php"><span class="glyphicon glyphicon-user"></span>&nbsp;View Profile</a></li>
                 <li><a href="logout.php?logout=true"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Sign Out</a></li>
               </ul>
             </li>
           </ul>
-        </div><!--/.nav-collapse -->
+        </div>
       </div>
     </nav>
 
 
     <div class="clearfix"></div>
-    	
+      
     
 <div class="container-fluid" style="margin-top:80px;">
-	
+  
     <div class="container">
     
-    	<label class="h5">Welcome : <?php print($userRow['full_name']); ?></label>
+      <label class="h5">Welcome : <?php print($userRow['full_name']); ?></label>
         <hr />
+        <?php 
+        while($userRows=$stmts->fetch()){ ?>
       <label class="h5">You have Assigned <?php print($userRows['work']); ?> </label>
-      <label class="h5">To <?php print($userRowss['full_name']); ?></label>
+      <label class="h5">To <?php print($userRows['full_name']); ?></label>
         <hr />
-        
-        <p class="h4">User Home Page</p> 
+        <?php }?>
     
     </div>
 
